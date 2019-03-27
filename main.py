238e4c1d8907a9a211fe8a3f3ff00d4946e0fedc
Weijing Wang <weijingrocks@gmail.com>
2019-03-26 22:58:39 -0700

    level settings. issue add more levels

--- main.py
+++ main.py
@@ -1,38 +1,71 @@
 class player():
 	"""docstring for player"""
-	def __init__(self, image, x, y):
+	def __init__(self, image, x, y,tileBoxes):
 		self.image = image
 		self.x = x
 		self.y = y
-
+		self.dx = 0
+		self.dy = 0
+		self.rect = pygame.Rect(self.x, self.y, 49, 49)
+		self.tileBoxes = tileBoxes
 	def draw(self, surface):
 		player = surface.blit(self.image, (self.x,self.y))
-		playerRect = pygame.Rect(self.x, self.y, 49, 49)
-		return playerRect
+		
+		return self.rect
 
 	def move(self):
 		pressed = pygame.key.get_pressed()
 		self.draw(screen)
 		if pressed[pygame.K_LEFT]:
-			self.x -=10
+			self.dx = -10
+			self.x +=self.dx
 			# print("left"+str(self.x))
 			self.draw(screen)
 		elif pressed[pygame.K_RIGHT]:
-			self.x+=10	
+			self.dx = 10
+			self.x +=self.dx	
 			# print("right"+str(self.x))
 			self.draw(screen)
 		elif pressed[pygame.K_UP]:
-			self.y-=10	
+			self.dy = -10
+			self.y +=self.dy
 			# print("up"+str(self.y))
 			self.draw(screen)
 		elif pressed[pygame.K_DOWN]:
-			self.y+=10
+			self.dy = 10
+			self.y +=self.dy
 			# print("down"+str(self.y))
 			self.draw(screen)
+
+		if self.rect.colliderect(self.tileBoxes[1]):
+			print("out")
+			return "done"
+
+		for wall in self.tileBoxes[0]:
+			if self.rect.colliderect(wall):
+				if self.dx > 0:
+					self.rect.right = wall.left
+					print("left")
+					self.x = wall[0]+100
+				elif self.dx < 0:
+					self.rect.left = wall.right
+					print("r")
+					self.x = wall[0]+50
+				elif self.dy > 0:
+					self.rect.bottom = wall.top
+					print("t")
+					self.y = wall[1]-50
+				elif self.dy < 0:
+					self.rect.top = wall.bottom
+					print("b")
+					self.y = wall[1]+50
+					
 
 		# elif playerRect.colliderect(otherRect) == True:
 		# 	print("eargsthd")
 		return(self.x, self.y)
+
+
 
 # class tile():
 # 	"""docstring for tile"""
@@ -59,11 +92,15 @@ class level():
 		self.tileX = tileX
 		self.tileY = tileY
 
-	def draw(self, tileX, tileY, tile):
+	def draw(self, tileX, tileY, tile, wall):
 
 		screen.blit(tile, (tileX,tileY))
 	#include tile properties
 		tileRect = pygame.Rect(tileX, tileY, 50, 50)
+		if wall == True:
+			tileRect = pygame.Rect(tileX, tileY, 50, 50)
+		else:
+			pass
 		return tileRect
 
 	def render_level(self,tile):
@@ -76,15 +113,19 @@ class level():
 			for item in row:
 				# print (item)`
 				if item == 0:
-					hi=self.draw(self.tileX, self.tileY, tile[0])
+					hi=self.draw(self.tileX, self.tileY, tile[0],False)
+					
 				elif item == 1:
-					hi=self.draw(self.tileX, self.tileY, tile[1])
+					hi=self.draw(self.tileX, self.tileY, tile[1],True)
+					tileRect_list.append(hi)
 				elif item == 2:
-					hi=self.draw(self.tileX, self.tileY, tile[2])
+					hi=self.draw(self.tileX, self.tileY, tile[2],False)
+					endTile = self.draw(self.tileX, self.tileY, tile[2],False)
 				elif item == 3:
-					hi=self.draw(self.tileX, self.tileY, tile[3])
+					hi=self.draw(self.tileX, self.tileY, tile[3],False)
+					
 
-				tileRect_list.append(hi)
+				
 				# print(hi)
 				self.tileX+=50
 
@@ -92,7 +133,7 @@ class level():
 			self.tileY+=50
 			# print("good")
 			
-		return tileRect_list
+		return tileRect_list ,endTile
 
 
 """This is where my program starts """
@@ -104,33 +145,31 @@ done = False
 
 #one = [[0,1],[2,3]]
 
-one =  [[0,1,2,3,0,1,2,3,1,1,0,1,1,1,1,1],
+one =  [[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
 		[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
 		[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
 		[1,0,0,1,3,1,0,0,0,0,0,0,0,0,0,1],
 		[1,0,0,1,3,1,0,0,0,0,0,0,0,0,0,1],
 		[1,0,0,1,3,1,0,0,0,0,0,0,0,0,0,1],
 		[1,0,0,1,3,1,0,0,0,0,0,0,0,0,0,1],
-		[1,0,0,1,3,1,0,0,0,0,0,0,0,0,0,1],
-		[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-		[1,0,0,0,0,0,0,0,0,0,0,0,0,2,0,1],
-		[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-		[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]]
+		[1,0,0,1,3,1,0,0,0,3,0,0,0,0,0,3],
+		[1,0,0,0,0,0,0,0,0,0,3,3,0,0,3,3],
+		[1,0,0,0,0,0,0,0,0,0,3,0,3,0,3,2],
+		[1,0,0,0,0,0,0,0,0,0,0,0,0,3,3,3],
+		[1,1,1,1,1,1,1,1,1,1,1,1,1,3,3,3]]
 
 
 
-myimage = pygame.image.load("myimage.png")
+myimage = pygame.image.load("b-ball.png")
 door_tile = pygame.image.load("tile.png") #door
 tile_tile =pygame.image.load("tile1.png") #tile
 wall_tile =pygame.image.load("tile2.png") #wall	
 light_tile =pygame.image.load("tile3.png") #light	
 
 
-bob=player(myimage,30,9) #player
-bob.draw(screen)
-
 
 tiles = (tile_tile, wall_tile, door_tile, light_tile)
+
 
 level_one = level(one, tiles,0,0) #level
 
@@ -138,47 +177,33 @@ level_one = level(one, tiles,0,0) #level
 
 x_collide = False
 y_collide = False
-
-i = 0
+tileBox = level_one.render_level(tiles)
+bob=player(myimage,50,50,tileBox) #player
+bob.draw(screen)
 
 while not done:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			done = True
 
+	tileBox = level_one.render_level(tiles)
+
+
 	# level_one.render_level(tiles)
 
 	#screen.fill((0,0,255))
 	temp_coor = bob.move()
 	playerBox = bob.draw(screen)
-	tileBox = level_one.render_level(tiles)
+	
 
-	for box in tileBox:
-		if playerBox.colliderect(box):
-			# print("asdf")
-			pass
-	# tile_temp_coor = level_one.render_level(tiles)
 
-	bob=player(myimage,temp_coor[0],temp_coor[1])
+
+
+
+	tile_temp_coor = level_one.render_level(tiles)
+
+	bob=player(myimage,temp_coor[0],temp_coor[1],tileBox)
 	bob.draw(screen)
 
-	# i+=1
-	# if (i == 5):
-	# 	break
-
-	# for x in tile_temp_coor[0]:
-	# 	if temp_coor[0] == tile_temp_coor[0]:
-	# 		x_collide = True
-	# 		print("x")
-	# 	else:
-	# 		x_collide = False
-	# for y in tile_temp_coor[1]:
-	# 	if temp_coor[1] == tile_temp_coor[1]:
-	# 		y_collide = True
-	# 		print("y")
-	# 	else:
-	# 		y_collide = False
-	# if x_collide == True and y_collide == True:
-	# 	print("collision")
 
 	pygame.display.flip()
